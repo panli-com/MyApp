fdaa9ba3fe6baa6b968bbd0f3f515abce3bea003
zanjs <root@zanjs.com>
2015-11-03 17:40:22 +0800

    login ui

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -3,6 +3,34 @@
 import React, { PropTypes, Component } from 'react';
 import styles from './LoginPage.css';
 import withStyles from '../../decorators/withStyles';
+import Link from '../Link';
+const mui = require('material-ui');
+
+const {
+  Checkbox,
+  ClearFix,
+  DatePicker,
+  Dialog,
+  DropDownMenu,
+  FlatButton,
+  FloatingActionButton,
+  LeftNav,
+  MenuItem,
+  Mixins,
+  Paper,
+  RadioButton,
+  RadioButtonGroup,
+  RaisedButton,
+  Snackbar,
+  Slider,
+  Styles,
+  Tabs,
+  Tab,
+  TextField,
+  Toggle,
+} = mui;
+
+let { Colors } = Styles;
 
 @withStyles(styles)
 class LoginPage extends Component {
@@ -14,11 +42,42 @@ class LoginPage extends Component {
   render() {
     const title = '登录';
     this.context.onSetTitle(title);
+    let styles = {
+      group: {
+        width: '100%',
+        float: 'left',
+        marginBottom: 32,
+      },
+      textfield: {
+        marginTop: 24,
+      },
+    };
     return (
       <div className="LoginPage">
         <div className="LoginPage-container">
           <h1>{title}</h1>
           <p>...</p>
+            <TextField
+                  style={styles.textfield}
+                  hintText="用户名"
+                  underlineFocusStyle={{borderColor: Colors.amber900}}
+                  defaultValue="" /><br/>
+            <TextField
+              hintText="密码"
+              floatingLabelText="Password"
+
+              type="password" /><br/>
+            <RaisedButton
+               style={styles.textfield}
+               label="登录" secondary={true} /><br/>
+             <FlatButton
+               style={styles.textfield}
+               linkButton={true}
+               href="/register"
+               secondary={true}
+               onClick={Link.handleClick}
+               label="注册" />
+
         </div>
       </div>
     );
--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -3,6 +3,33 @@
 import React, { PropTypes, Component } from 'react';
 import withStyles from '../../decorators/withStyles';
 import styles from './RegisterPage.css';
+const mui = require('material-ui');
+
+const {
+  Checkbox,
+  ClearFix,
+  DatePicker,
+  Dialog,
+  DropDownMenu,
+  FlatButton,
+  FloatingActionButton,
+  LeftNav,
+  MenuItem,
+  Mixins,
+  Paper,
+  RadioButton,
+  RadioButtonGroup,
+  RaisedButton,
+  Snackbar,
+  Slider,
+  Styles,
+  Tabs,
+  Tab,
+  TextField,
+  Toggle,
+} = mui;
+
+let { Colors } = Styles;
 
 @withStyles(styles)
 class RegisterPage extends Component {
@@ -14,11 +41,39 @@ class RegisterPage extends Component {
   render() {
     const title = '注册一个账号';
     this.context.onSetTitle(title);
+    let styles = {
+      group: {
+        width: '100%',
+        float: 'left',
+        marginBottom: 32,
+      },
+      textfield: {
+        marginTop: 24,
+      },
+    };
     return (
       <div className="RegisterPage">
         <div className="RegisterPage-container">
           <h1>{title}</h1>
-          <p>...</p>
+          <p>------</p>
+            <TextField
+                  style={styles.textfield}
+                  hintText="用户名"
+                  underlineFocusStyle={{borderColor: Colors.amber900}}
+                  defaultValue="" /><br/>
+            <TextField
+                  style={styles.textfield}
+                  hintText="邮箱"
+                  underlineFocusStyle={{borderColor: Colors.amber900}}
+                  defaultValue="" /><br/>
+            <TextField
+              hintText="密码"
+              floatingLabelText="Password"
+              
+              type="password" /><br/>
+            <RaisedButton
+               style={styles.textfield}
+               label="注册" />
         </div>
       </div>
     );
